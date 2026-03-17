c849f100a7213f3a79f71e6d78a49ecc1b64eadd
lum-i3 <dana.bahena.21@gmail.com>
2026-03-17 14:43:27 -0600

    Creacion de proyecto

---
+++ src/test/java/com/example/integradora5d/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.integradora5d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
